580d460b77675df0994eb6f49d375c688ba77746
Cristian-Veliz <cristianveliz6@gmail.com>
2023-11-04 00:07:29 -0300

    2doCommit

--- src/views/Landing/Landing.jsx
+++ src/views/Landing/Landing.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-
-
+import React from "react";
+import { Link } from "react-router-dom";
+import image from "../../assets/imgLanding.jpg";
+import style from "./Landing.module.css";
 
 function Landing() {
   return (
-    <div>
+    <div className={style.container}>
+      {/* <img src={image} alt="imageLanding" className={style.image} /> */}
       <h1 style={{ color: "#e55743" }}>Hola, Soy Cristian Veliz</h1>
-      <h2 >¡Bienvenido a mi Porfolio!</h2>
-  </div>
-  )
+      <h2>¡Bienvenido a mi Porfolio!</h2>
+      <Link to="/home">
+        <button className={style.button}>Ingresar</button>
+      </Link>
+    </div>
+  );
 }
 
 export default Landing;
-
- 
