0cce2d1e529fa8d1b7158d00ce4077c700039dac
Cristian-Veliz <cristianveliz6@gmail.com>
2023-11-10 21:16:15 -0300

    Home1erCommit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { Routes, Route, useLocation } from 'react-router-dom'
 import Landing from './views/Landing/Landing.jsx';
 import NavBarLanding from './components/NavBarLanding/NavBarLanding';
+import Home from './views/Home.jsx/Home';
 
 
 function App() {
@@ -12,9 +13,10 @@ function App() {
       {pathname !== '/' ? <NavBarLanding/> : null }
       <Routes>
       <Route path='/' element={<Landing/>}/>
+      <Route path='/home' element={<Home/>}/>
       </Routes>      
     </div>
   )
 }
 
-export default App ;
+export default App;
---
+++ src/views/Home.jsx/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <h1>Estoy en mi Home</h1>
+    </div>
+  )
+}
+
+export default Home
