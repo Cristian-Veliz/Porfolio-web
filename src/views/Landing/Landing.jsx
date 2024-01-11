7c0c5e770c172e567c8acc3eefe4e720560fb2a0
Cristian-Veliz <cristianveliz6@gmail.com>
2024-01-10 22:44:18 -0300

    ParaDeploy

--- src/views/Landing/Landing.jsx
+++ src/views/Landing/Landing.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import image from "../../assets/imgLanding.jpg";
 import style from "./Landing.module.css";
 
 function Landing() {
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-// vite.config.js
-import react from '@vitejs/plugin-react';
+// // vite.config.js
+// import react from '@vitejs/plugin-react';
 
-export default {
-  plugins: [react()],
-  base: 'http://localhost:5173', // Ajusta esto según tu necesidad
-  build: {
-    rollupOptions: {
-      external: ['@formspree/react'],
-    },
-  },
-};
+// export default {
+//   plugins: [react()],
+//   base: 'http://localhost:5173', // Ajusta esto según tu necesidad
+//   build: {
+//     rollupOptions: {
+//       external: ['@formspree/react'],
+//     },
+//   },
+// };
 
 
 
@@ -23,3 +23,12 @@ export default {
 //     },
 //   },
 // };
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
