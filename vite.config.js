173406f04f419fed8dd4d21c4bf127d7beb8f8fc
Cristian-Veliz <cristianveliz6@gmail.com>
2024-01-10 21:49:33 -0300

    Cambio01

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default {
   plugins: [react()],
-  base: 'Porfolio-web', // Ajusta esto según tu necesidad
+  base: 'http://localhost:5173', // Ajusta esto según tu necesidad
   build: {
     rollupOptions: {
       external: ['@formspree/react'],
