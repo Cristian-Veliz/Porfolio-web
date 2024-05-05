1f925b283956c393caabbf69782a728bac230eeb
Cristian-Veliz <cristianveliz6@gmail.com>
2024-05-05 20:47:01 -0300

    logoTypeScript

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -95,6 +95,13 @@ function Home() {
                 alt=""
               />
             </div>
+            <div className={style.TypeScript}>
+              <img
+                className={style["img-TypeScript"]}
+                src="https://media.printables.com/media/prints/744766/images/5814515_854c67bc-6861-4faf-a006-2ea2e865b8ec_d4eec15e-d179-4f60-9b8d-25a82897e60f/thumbs/cover/800x800/png/ts-logo-512.webp"
+                alt="Imagen TypeScript"
+              />
+            </div>
           </div>
         </p>
       </div>
